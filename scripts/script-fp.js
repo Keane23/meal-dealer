c500d28f1d99b73951f54a812e2889f4183982f7
Jordan <jordanliman23@gmail.com>
2025-02-17 15:21:07 +0000

    Adjusted the filter page to save data from the filters as a dictionary

--- scripts/script-fp.js
+++ scripts/script-fp.js
@@ -1,3 +1,7 @@
+/*
+Javascript code for the double input sliders
+*/
+
 window.onload = function(){
     slideOne();
     slideTwo();
@@ -39,4 +43,80 @@ function fillColor(){
     percent1 = (sliderOne.value / sliderMaxValue) * 100;
     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
     sliderTrack.style.background = `linear-gradient(to right, #163446 ${percent1}% , #B0CFDF ${percent1}% , #B0CFDF ${percent2}%, #163446 ${percent2}%)`;
+}
+
+/*
+Javascript code for storing the data from the filter page
+*/
+
+let nutrients = {
+"calorieRange": [],
+"proteinRange": [],
+"carbohydrateRange": [],
+"fatRange": [],
+"saturatedFatRange": [],
+"sugarRange": [],
+"fibreRange": [],
+"saltRange": [],
+"vegetarianBool": false,
+"veganBool": false,
+"halalBool": false,
+"glutenFreeBool": false,
+"lactoseIntolerantBool": false,
+"nutAllergyBool": false,
+}
+
+    function findProducts() {
+        nutrients["calorieRange"] = [Number(document.getElementById("range1").textContent), Number(document.getElementById("range2").textContent)]
+        nutrients["proteinRange"] = [Number(document.getElementById("range3").textContent), Number(document.getElementById("range4").textContent)]
+        nutrients["carbohydrateRange"] = [Number(document.getElementById("range5").textContent), Number(document.getElementById("range6").textContent)]
+        nutrients["fatRange"] = [Number(document.getElementById("range7").textContent), Number(document.getElementById("range8").textContent)]
+        nutrients["saturatedFatRange"] = [Number(document.getElementById("range9").textContent), Number(document.getElementById("range10").textContent)]
+        nutrients["sugarRange"] = [Number(document.getElementById("range11").textContent), Number(document.getElementById("range12").textContent)]
+        nutrients["fibreRange"] = [Number(document.getElementById("range13").textContent), Number(document.getElementById("range14").textContent)]
+        nutrients["saltRange"] = [Number(document.getElementById("range15").textContent), Number(document.getElementById("range16").textContent)]
+        nutrients["vegetarianBool"] = document.getElementById("vegetarian").checked
+        nutrients["veganBool"] = document.getElementById("vegan").checked
+        nutrients["halalBool"] = document.getElementById("halal").checked
+        nutrients["glutenFreeBool"] = document.getElementById("gluten-free").checked
+        nutrients["lactoseIntolerantBool"] = document.getElementById("lactose-intolerant").checked
+        nutrients["nutAllergyBool"] = document.getElementById("nut-allergy").checked
+
+}
+
+function nextPage() {
+    window.location.href = "meal-options-page.html"
+}
+
+/*
+Javascript code for showing the meal options created from the filter
+*/
+
+function showCard() {
+    window.location.href = "display-card-page.html"
+}
+
+function displayMealOptions() {
+    
+    for (let i = 1; i < 6; i++) {
+        document.getElementById(`meal__slide${i}`).innerHTML =
+        `<div class="carousel__snapper">
+            <a href="#meal__slide${i === 1 ? i + 4 : i - 1}" class="carousel__prev">Go to last slide</a>Stupid
+            <a href="#meal__slide${i === 5 ? i - 4 : i + 1}" class="carousel__next">Go to next slide</a>
+        </div>`
+    }
+    for (let i = 1; i < 6; i++) {
+        document.getElementById(`snacks__slide${i}`).innerHTML =
+        `<div class="carousel__snapper">
+            <a href="#snacks__slide${i === 1 ? i + 4 : i - 1}" class="carousel__prev">Go to last slide</a>Stupid
+            <a href="#snacks__slide${i === 5 ? i - 4 : i + 1}" class="carousel__next">Go to next slide</a>
+        </div>`
+    }
+    for (let i = 1; i < 6; i++) {
+        document.getElementById(`drink__slide${i}`).innerHTML =
+        `<div class="carousel__snapper">
+            <a href="#drink__slide${i === 1 ? i + 4 : i - 1}" class="carousel__prev">Go to last slide</a>Stupid
+            <a href="#drink__slide${i === 5 ? i - 4 : i + 1}" class="carousel__next">Go to next slide</a>
+        </div>`
+    }
 }
